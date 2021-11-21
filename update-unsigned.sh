0eaecb782d025776986b41455736356cc308e605
Ted Winograd <ted.winograd@gmail.com>
2021-11-21 13:27:30 -0500

    Adding an all architecture for a cross-platform package I use

--- update-unsigned.sh
+++ update-unsigned.sh
@@ -5,10 +5,11 @@ wget "$2" -q -O /var/www/html/debian/pool/main/$3
         
 # This will give off some warnings, but it's usable
 apt-ftparchive packages . > dists/stable/main/binary-amd64/Packages
+dpkg-scanpackages -a all . > dists/stable/main/binary-all/Packages
 cd dists/stable
         
 # Ubuntu software updater will be weird without these pieces
-apt-ftparchive -oAPT::FTPArchive::Release::Suite=stable -oAPT::FTPArchive::Release::Codename=stable -oAPT::FTPArchive::Release::Architectures=amd64 -oAPT::FTPArchive::Release::Components=main release . > Release
+apt-ftparchive -oAPT::FTPArchive::Release::Suite=stable -oAPT::FTPArchive::Release::Codename=stable -oAPT::FTPArchive::Release::Architectures=amd64,all -oAPT::FTPArchive::Release::Components=main release . > Release
 
 # Create the signed Release files
 gpg --default-key $1 --pinentry-mode loopback --passphrase-file ~/zoom_passphrase.txt --batch --yes -abs -o Release.gpg Release
--- update-zoom.sh
+++ update-zoom.sh
@@ -41,10 +41,14 @@ if [ "$(md5sum -c ~/zoom.md5)" != "/tmp/zoom_amd64.deb: OK" ]; then
         
         # This will give off some warnings, but it's usable
         apt-ftparchive packages . > dists/stable/main/binary-amd64/Packages
+
+        # This covers some cross-platform packages that I use for the 
+        # Raspberry Pi
+        dpkg-scanpackages -a all . > dists/stable/main/binary-all/Packages
         cd dists/stable
         
         # Ubuntu software updater will be weird without these pieces
-        apt-ftparchive -oAPT::FTPArchive::Release::Suite=stable -oAPT::FTPArchive::Release::Codename=stable -oAPT::FTPArchive::Release::Architectures=amd64 -oAPT::FTPArchive::Release::Components=main release . > Release
+        apt-ftparchive -oAPT::FTPArchive::Release::Suite=stable -oAPT::FTPArchive::Release::Codename=stable -oAPT::FTPArchive::Release::Architectures=amd64,all -oAPT::FTPArchive::Release::Components=main release . > Release
 
         # Create the signed Release files
         gpg --default-key $1 --pinentry-mode loopback --passphrase-file ~/zoom_passphrase.txt --batch --yes -abs -o Release.gpg Release
